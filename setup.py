0e9a60e1d5f737702cb8a3fb3a32782ced9973e6
Mathew Topper <damm_horse@yahoo.co.uk>
2020-04-08 17:28:41 +0100

    Add Travis CI support
    This commit is for testing CI on travis

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(name='convergence',
       url='https://github.com/H0R5E/convergence',
       packages=['convergence'],
       python_requires='>=2.7, !=3.9.*, <4',
+      extras_require={
+        'test': ["pytest", "pytest-cov"]},
       entry_points = {'console_scripts': 
                            ['grid-convergence = convergence:main']},
       classifiers=[
@@ -23,7 +25,6 @@ setup(name='convergence',
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
-        Programming Language :: Python :: 3
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
